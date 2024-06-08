6bf8763c5df2d82dcd1333aa605140454406285a
Paul Nwabudike <chindinma@gmail.com>
2024-06-08 23:19:02 +0100

    Register commandHandlers in main application class of account.cmd project

--- bank-account/account.cmd/src/main/java/com/tecbank/account/cmd/CommandApplication.java
+++ bank-account/account.cmd/src/main/java/com/tecbank/account/cmd/CommandApplication.java
@@ -1,13 +1,31 @@
 package com.tecbank.account.cmd;
 
+import com.tecbank.account.cmd.api.commands.*;
+import com.tecbank.cqrs.core.Infrastructure.CommandDispatcher;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommandApplication {
 
+	@Autowired
+	private CommandDispatcher commandDispatcher;
+	@Autowired
+	private CommandHandler commandHandler;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CommandApplication.class, args);
 	}
+
+	@PostConstruct
+	public void registerHandlers() {
+		commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+	}
+
 
 }
